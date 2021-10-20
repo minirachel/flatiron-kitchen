a40e938b23cef06a78f75653eb08783cc5d7ea98
minirachel <rachel.a.levinson@gmail.com>
2021-10-20 18:34:26 -0400

    Done.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,3 +1,21 @@
 class IngredientsController < ApplicationController
+    def new
 
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def show
+
+    end
+
+    def ingredient_params
+        params.require(:ingredient).permit(:name)
+    end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,21 @@
 class RecipesController < ApplicationController
+    def new
 
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def show
+
+    end
+
+    def recipe_params
+        params.require(:recipe).permit(:name)
+    end
 end
